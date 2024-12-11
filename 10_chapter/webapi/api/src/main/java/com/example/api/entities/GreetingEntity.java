8e53af13fdb19a172fffc1e467b4b365b29a313b
hiroya4818 <osaka481859@gmail.com>
2024-12-11 23:40:36 +0900

    簡単なアプリをコンテナ内で作成

---
+++ 10_chapter/webapi/api/src/main/java/com/example/api/entities/GreetingEntity.java
@@ -0,0 +1,13 @@
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "greetings")
+public class GreetingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String lang;
+    public String text;
+}
